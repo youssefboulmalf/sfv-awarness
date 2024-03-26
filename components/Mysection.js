83b38466919d77b76d603bfa9da5317006c2d359
youssefboulmalf <youssefboulmalf@gmail.com>
2024-03-26 18:34:20 +0100

    fixed no image bug

--- components/Mysection.js
+++ components/Mysection.js
@@ -3,9 +3,6 @@ import Image from "next/image";
 import styles from "./Mysection.module.scss";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
-import back from "../components/icons/back.png"
-
-import { IoHeartOutline } from "react-icons/io5";
 
 gsap.registerPlugin(ScrollTrigger);
 
